3e824b165cff3fa6a0e128433489e5b771f0c408
Anant S <startanant@gmail.com>
2020-03-28 20:14:35 -0400

    Updates

---
+++ precache-manifest.14d660d89f0633a250c6c18e4f82fae0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd7823139cf5acfc299c79d0e643b8d",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "4de471dff8a4c8a3aeec",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79adb18552a7d203a215",
+    "url": "/react-employee-directory/static/js/2.0ee4e0b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.0ee4e0b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de471dff8a4c8a3aeec",
+    "url": "/react-employee-directory/static/js/main.444ed3b9.chunk.js"
+  },
+  {
+    "revision": "04244e7005e34b33cd29",
+    "url": "/react-employee-directory/static/js/runtime-main.a0c97ca5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
