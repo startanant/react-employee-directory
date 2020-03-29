596dda48cda08c56bf87d2d3dff19d9460216598
Anant S <startanant@gmail.com>
2020-03-28 21:28:47 -0400

    Moves Table out into component:Table

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './Components/Table'
 
 function App() {
   return (
     <div className="App">
-      <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </div>
   );
 }
