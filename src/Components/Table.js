33bebd6b252a01f8fed56f6ca0cf911b5aff1a1d
Anant S <startanant@gmail.com>
2020-03-28 23:26:38 -0400

    Adds sortById(): sort table data by Id asc/desc

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import employees from '../employees.json';
+
 
 function Table() {
 
-    const employeeList = employees;
-    console.log(employeeList);
+    const [ employeeList, setEmployeeList ] = useState( employees );
+    const [ sort, setSort ] = useState ('asc');
+    
+    function sortById(sort) {
+        let sortedEmployeeList;
+        if(sort === 'asc') {
+            sortedEmployeeList = employeeList.sort((a, b) => (a.id > b.id) ? -1 : 1);
+            setSort('desc');
+        } else {
+            sortedEmployeeList = employeeList.sort((a, b) => (a.id > b.id) ? 1 : -1);
+            setSort('asc');
+        }
+        setEmployeeList([...sortedEmployeeList]);
+    }
 
     return (
         <table className="table table-dark">
             <thead>
                 <tr>
-                    <th scope="col">id</th>
+                    <th scope="col" onClick={function () { sortById(sort)} }>id</th>
                     <th scope="col">Name</th>
                     <th scope="col">Email</th>
                     <th scope="col">Phone</th>
