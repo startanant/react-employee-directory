f343c81b8b81a6788560cb09f970154e6d2908a4
Anant S <startanant@gmail.com>
2020-03-30 20:18:16 -0400

    Adds effect to filter by employee name

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import employees from '../employees.json';
 
 
 function Table() {
 
-    const [ employeeList, setEmployeeList ] = useState( employees );
-    const [ sort, setSort ] = useState ('asc');
+    const [ employeeList, setEmployeeList ] = useState( [] );
+    const [ sort, setSort ] = useState( 'asc' );
+    const [ myFilter, setFilter] = useState( '' );
+
+    useEffect(
+        function() {
+            console.log(`[useEffect] was run for filter(${myFilter})`);
+            loadEmployees( myFilter );
+        }, [myFilter]
+    );
+
+    function loadEmployees( filter ) {
+        let filteredEmployeeList;
+        if (filter === '') {
+            filteredEmployeeList = employees;
+        } else {
+            filteredEmployeeList = employees.filter( employee =>  employee.name.toLowerCase().startsWith(filter) );
+        }
+        console.log(filteredEmployeeList);
+        setEmployeeList([...filteredEmployeeList]);
+    }
     
-    function sortById(sort) {
+    function sortById( sort ) {
         let sortedEmployeeList;
         if(sort === 'asc') {
             sortedEmployeeList = employeeList.sort((a, b) => (a.id > b.id) ? -1 : 1);
@@ -19,12 +38,26 @@ function Table() {
         setEmployeeList([...sortedEmployeeList]);
     }
 
+    // function filter(value) {
+    //     //alert(value);
+    //     let filteredEmployeeList = employeeList.filter(employee => { let emp_name = employee.name.toLowerCase(); console.log(emp_name); return emp_name.startsWith(value) });
+    //     console.log(filteredEmployeeList);
+    //     setEmployeeList([...filteredEmployeeList]);
+    // }
+
+    function handleInputChange(event) {
+        //alert(event.target.value);
+        const value = event.target.value.toLowerCase();
+        //filter(value);
+        setFilter(value);
+    }
+
     return (
         <table className="table table-dark">
             <thead>
                 <tr>
                     <th scope="col" onClick={function () { sortById(sort)} }>id</th>
-                    <th scope="col">Name</th>
+                    <th scope="col"><input type="text" value={myFilter} placeholder="Name" onChange={handleInputChange}></input></th>
                     <th scope="col">Email</th>
                     <th scope="col">Phone</th>
                 </tr>
